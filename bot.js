02628a7e74186184026b818be253d575545508fa
Wetzu212 <34999927+Wetzu212@users.noreply.github.com>
2018-01-21 13:57:23 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,13 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setGame(` >help ${client.guilds.size}`)
 });
+
+client.on('message', message => {
+ if (message.author.bot) return;
+ if (!message.content.startsWith(prefix)) return;
+}
 
 client.on('message', message => {
     if (message.content.startsWIth 'ping') {
